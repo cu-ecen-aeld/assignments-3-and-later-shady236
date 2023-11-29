4abf26887a52f445c3f0b53e6dabd8f58a244369
Shady Mansour <shadymansour19@gmail.com>
2023-11-29 21:24:52 +0200

    Done :)

--- examples/systemcalls/systemcalls.h
+++ examples/systemcalls/systemcalls.h
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <unistd.h>
+#include <unistd.h>
 
 bool do_system(const char *command);
 
