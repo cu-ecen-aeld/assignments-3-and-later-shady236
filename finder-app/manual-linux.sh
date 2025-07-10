fbaa506d21bbcf71ab6cd844069976ca7931648b
= <=>
2025-07-11 00:39:31 +0300

    add debencies in deb directory

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -38,7 +38,7 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE mrproper			# deep clean
     make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE defconfig			# default condig
     make -j4 ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE all			# build
-    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE modules
+    # make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE modules
     make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE dtbs				# device tree build
 fi
 
@@ -87,10 +87,8 @@ ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
-cp /opt/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 "$OUTDIR/rootfs/lib/"
-cp /opt/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libm.so.6 "$OUTDIR/rootfs/lib64/"
-cp /opt/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 "$OUTDIR/rootfs/lib64/"
-cp /opt/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libc.so.6 "$OUTDIR/rootfs/lib64/"
+cp -r "$FINDER_APP_DIR/../deb/lib" "$OUTDIR/rootfs/lib/"
+cp -r "$FINDER_APP_DIR/../deb/lib64" "$OUTDIR/rootfs/lib64/"
 
 
 # TODO: Make device nodes
